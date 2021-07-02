9ac28577e85bb44bc188661fea606f97ea2d7b83
Amanda Nelson <amanda.gaylord@gmail.com>
2021-07-02 15:51:50 -0400

    new server created

---
+++ data/server.js
@@ -0,0 +1,32 @@
+//dependencies to help the backend:
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+//routers:
+const projectRouter = require("./routers/projectRouter");
+
+const server = express();
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+
+server.use("/api/projects", projectRouter);
+
+server.get("/", async (req, res, next) => {
+  try {
+    res.status(200).json({
+      message: "Welcome to the backend - Things seem to be working so far.",
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+//Catch all for 500 server errors:
+server.use((err, req, res, next) => {
+  console.log(`500 Error: ${err}`);
+  res.status(500).json({
+    message: "Oops! Something went wrong!",
+  });
+});
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const pool = require("./db");
-const path = require("path");
-const PORT = process.env.PORT || 3333;
+require('dotenv').config()
 
-app.use(cors());
-app.use(express.json());
+const path = require('path')
+const express = require('express')
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/build")));
-}
+const server = require('./api/server')
 
-//insert the routes for projects (just need a GET)
-app.get("/projects", async (req, res) => {
-  try {
-    const allProjects = await pool.query("SELECT * FROM projects");
-    res.json(allProjects.rows);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
+const port = process.env.PORT
 
-app.listen(PORT, () => {
-  console.log(`server has started on http://localhost:${PORT}`);
-});
+server.use(express.static(path.join(__dirname, 'client/dist')))
+
+server.get('*', (req, res) => {
+  // if you want to serve a SPA using Express you totally can!
+  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'))
+})
+
+server.listen(port, () => {
+  console.log('listening on ' + port)
+})
